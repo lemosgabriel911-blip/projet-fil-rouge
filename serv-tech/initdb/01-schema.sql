eaeb99fc784da455865a28fbd636c7a224d054f5
Gabriel <nelson.ferrazlemos@heig-vd.ch>
2026-03-20 13:18:46 +0100

    Création des tables, màj du MLD

--- serv-tech/initdb/01-schema.sql
+++ serv-tech/initdb/01-schema.sql
@@ -1,74 +1,97 @@
 -------------- TABLES DE REFERENCE --------------
-CREATE TABLE type_mobilier(
+CREATE TABLE type_mobilier (
     id SERIAL PRIMARY KEY,
-    libelle VARCHAR(100)
-)
+    libelle VARCHAR(100) NOT NULL UNIQUE
+);
 
-CREATE TABLE type_materiel(
+CREATE TABLE type_materiel (
     id SERIAL PRIMARY KEY,
-    libelle VARCHAR(100)
-)
+    libelle VARCHAR(100) NOT NULL UNIQUE
+);
 
-CREATE TABLE type_interv(
+CREATE TABLE type_interv (
     id SERIAL PRIMARY KEY,
-    libelle VARCHAR(100)
-)
+    libelle VARCHAR(100) NOT NULL UNIQUE
+);
 
-CREATE TABLE signal_urgence(
+CREATE TABLE signal_urgence (
     id SERIAL PRIMARY KEY,
-    libelle VARCHAR(50)
-)
+    libelle VARCHAR(50) NOT NULL UNIQUE
+);
 
-CREATE TABLE signal_statut(
+CREATE TABLE signal_statut (
     id SERIAL PRIMARY KEY,
-    libelle VARCHAR(50)
-)
+    libelle VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE type_materiau(
+    id SERIAL PRIMARY KEY,
+    libelle VARCHAR(40) NOT NULL UNIQUE
+);
+
+CREATE TABLE etat_inventaire(
+    id SERIAL PRIMARY KEY,
+    libelle VARCHAR(30) NOT NULL UNIQUE
+);
 
 -------------- TABLES SERVTECH --------------
 
 CREATE TABLE fournisseur (
     id SERIAL PRIMARY KEY,
-    entreprise VARCHAR(80) NOT NULL UNIQUE
-    contact VARCHAR(80) NOT NULL UNIQUE
-    telephone INTEGER NOT NULL UNIQUE
-    email VARCHAR (80)NOT NULL UNIQUE
-    remarques VARCHAR (255)
-    # FK ID à mettre
-);
-
-CREATE TABLE interventions (
-    SERIAL PRIMARY KEY,
-    date_intervention DATE NOT NULL
-    objet
-    technicien
-    duree
-    materiel
-    remarques
-    # FK ID à mettre
+    entreprise VARCHAR(80) NOT NULL UNIQUE,
+    contact VARCHAR(80) NOT NULL UNIQUE,
+    telephone INTEGER NOT NULL UNIQUE,
+    email VARCHAR(80) NOT NULL UNIQUE,
+    remarques VARCHAR(255),
+    id_type_materiel INTEGER NOT NULL REFERENCES type_materiel(id)
 );
 
 CREATE TABLE inventaire (
-    SERIAL PRIMARY KEY,
-    type VARCHAR(80) NOT NULL UNIQUE
-    materiau
-    lieu
-    latitude
-    longitude
-    date_installation
-    etat
-    remarques
+    id SERIAL PRIMARY KEY,
+    lieu VARCHAR(80) NOT NULL,
+    latitude DECIMAL(10, 5) NOT NULL,
+    longitude DECIMAL(10, 5) NOT NULL,
+    date_installation DATE NOT NULL,
+    remarques VARCHAR(255),
+    id_type_mobilier INTEGER NOT NULL REFERENCES type_mobilier(id),
+    id_type_materiau INTEGER NOT NULL REFERENCES type_materiau(id),
+    id_etat_inventaire INTEGER NOT NULL REFERENCES etat_inventaire(id),
+    id_fournisseur INTEGER NOT NULL REFERENCES fournisseur(id)
+);
+
+
+CREATE TABLE interventions (
+    id SERIAL PRIMARY KEY,
+    date_intervention DATE NOT NULL,
+    objet VARCHAR(80) NOT NULL,
+    technicien VARCHAR(80) NOT NULL,
+    duree TIME NOT NULL,
+    cout_materiel INTEGER NOT NULL,
+    remarques VARCHAR(255),
+    id_type_interv INTEGER NOT NULL REFERENCES type_interv(id),
+    id_inventaire INTEGER NOT NULL REFERENCES inventaire(id)
 );
 
 CREATE TABLE signalement (
-    SERIAL PRIMARY KEY,
-    type VARCHAR(80) NOT NULL UNIQUE
-    data
-    signale_par
-    objet
-    description
-    urgence
-    statut
+    id SERIAL PRIMARY KEY,
+    date_signalement DATE NOT NULL,
+    signale_par VARCHAR(80),
+    objet VARCHAR (190),
+    description VARCHAR(80) NOT NULL,
+    id_signal_urgence INTEGER NOT NULL REFERENCES signal_urgence(id),
+    id_signal_statut INTEGER NOT NULL REFERENCES signal_statut(id)
 );
 
--------------- TABLES DE NORMALISATION --------------
+-------------- TABLES DE LIAISON --------------
 
+CREATE TABLE interv_signal (
+    id SERIAL PRIMARY KEY,
+    id_signalement INTEGER NOT NULL REFERENCES signalement(id),
+    id_interventions INTEGER NOT NULL REFERENCES interventions(id)
+);
+
+CREATE TABLE signal_invent (
+    id SERIAL PRIMARY KEY,
+    id_signalement INTEGER NOT NULL REFERENCES signalement(id),
+    id_inventaire INTEGER NOT NULL REFERENCES inventaire(id)
+);
