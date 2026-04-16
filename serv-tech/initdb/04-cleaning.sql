1b674b3cbb8dd5994558edcd1a174d6bda16727b
Gabriel <nelson.ferrazlemos@heig-vd.ch>
2026-04-16 14:24:32 +0200

    Création cleaning 1 et 2

--- serv-tech/initdb/04-cleaning.sql
+++ serv-tech/initdb/04-cleaning.sql
@@ -1,4 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS unaccent;
 
-SELECT DISTINCT
-FROM staging.fournisseur
+INSERT INTO
+    public.fournisseur (telephone)
+SELECT telephone
+FROM (
+        SELECT DISTINCT
+            CASE
+                WHEN lower(trim(telephone)) LIKE '%+41 21 456 78 90%' then 214567890
+                ELSE NULL
+            END AS telephone
+        FROM staging.fournisseur
+    )
+WHERE
+    telephone IS NOT NULL;
+
