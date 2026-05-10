70953b519a6b9df957220eb93b3c925cd78ea63a
Gabriel <nelson.ferrazlemos@heig-vd.ch>
2026-05-10 18:51:57 +0200

    Continuation du nettoyage 2.0

--- serv-tech/initdb/04-cleaning.sql
+++ serv-tech/initdb/04-cleaning.sql
@@ -60,36 +60,121 @@ SELECT DISTINCT (
 FROM staging.inventaire;
 -- Nettoyage inventaire etat
 
-INSERT INTO type_interv (libelle)
+WITH materiel_eclate AS (
+    SELECT
+        f.entreprise,
+        materiel_value
+    FROM staging.fournisseur f,
+         UNNEST(regexp_split_to_array(f.materiel, ', *')) AS materiel_value
+    WHERE f.materiel IS NOT NULL
+),
+
+materiel_normalise AS (
+    SELECT DISTINCT
+        entreprise,
+        CASE
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%bancs%' THEN 'bancs'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%bancs bois%' THEN 'bancs bois'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%poubelles%' THEN 'poubelles'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%peinture%' THEN 'peinture'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%bornes%' THEN 'bornes recharge EV'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%nettoyage%' THEN 'nettoy age'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%panneaux%' THEN 'panneaux'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%visserie%' THEN 'visserie'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%tables%' THEN 'tables'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%plantations autour mobilier%' THEN 'plantations'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%abris bus%' THEN 'abris bus'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%reprise ancien mobilier%' THEN 'reprise mobilier'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%tags%' THEN 'tags'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%fontaines%' THEN 'fontaines'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%conduites%' THEN 'conduites'
+            WHEN LOWER(TRIM(materiel_value)) LIKE '%vasques%' THEN 'vasques'
+            WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%bancs métal%' THEN 'bancs metal'
+            WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%éclairage LED%' THEN 'eclairage led'
+            WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%éclairage%' THEN 'eclairage'
+            WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%petites pièces%' THEN 'petites pieces'
+            ELSE 'non spécifié'
+        END AS libelle
+    FROM materiel_eclate
+)
+INSERT INTO public.fournisseur_materiel (fournisseur_id, type_materiel_id)
+SELECT DISTINCT
+    pf.id,
+    tm.id
+FROM materiel_normalise mn
+JOIN public.fournisseur pf ON pf.entreprise = mn.entreprise
+JOIN public.type_materiel tm ON mn.libelle = tm.libelle;
+
+-- A FAIRE nettoyage, éclatement et liaison fournisseur materiel
+
+INSERT INTO signal_urgence (libelle)
 SELECT DISTINCT (
     CASE
-        WHEN LOWER(TRIM("type_intervention")) LIKE '%peinture%' THEN 'peinture'
-        WHEN LOWER(TRIM("type_intervention")) LIKE '%hivernage%' THEN 'hivernage'
-        WHEN LOWER(TRIM("type_intervention")) LIKE '%remplacement%' THEN 'remplacement'
-        WHEN LOWER(TRIM("type_intervention")) LIKE '%remise en service%' THEN 'remise en service'
-        WHEN LOWER(TRIM("type_intervention")) LIKE '%nettoyage%' THEN 'nettoyage'
-        WHEN LOWER(TRIM(unaccent("type_intervention"))) LIKE '%redressage mat%' THEN 'redressage mat'
-        WHEN LOWER(TRIM(unaccent("type_intervention"))) LIKE '%detartrage%' THEN 'detartrage'
-        WHEN LOWER(TRIM(unaccent("type_intervention"))) LIKE '%reparation%' THEN 'reparation'
-        WHEN LOWER(TRIM(unaccent("type_intervention"))) LIKE '%mise a jour%' THEN 'mise a jour'
+        WHEN urgence IS NULL OR TRIM("urgence") = '' THEN 'non urgent'
+        WHEN LOWER(TRIM("urgence")) LIKE '%normal%' THEN 'urgence moderee'
+        WHEN LOWER(TRIM("urgence")) LIKE '%urgent%' THEN 'urgence majeure'
         ELSE 'non spécifié'
-     END ) AS type_interv_nettoye
-FROM staging.interventions;
--- Nettoyage intervention type
+     END ) AS signal_urgence_nettoye
+FROM staging.signalements;
+-- Nettoyage signal_urgence urgence 
 
-INSERT INTO type_materiel (libelle)
-SELECT DISTINCT
+INSERT INTO signal_statut (libelle)
+SELECT DISTINCT (
     CASE
-        WHEN LOWER(TRIM(materiel_value)) LIKE '%bancs%' THEN 'bancs'
-        WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%metal%' THEN 'metal'
-        WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%plastique%' THEN 'plastique'
-        WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%sodium%' THEN 'sodium'
-        WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%led%' THEN 'led'
-        WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%pierre%' THEN 'pierre'
-        WHEN LOWER(TRIM(unaccent(materiel_value))) LIKE '%beton%' THEN 'beton'
+        WHEN statut IS NULL OR TRIM("statut") = '' THEN 'a planifier'
+        WHEN LOWER(TRIM("statut")) LIKE '%en attente%' THEN 'en attente'
+        WHEN LOWER(TRIM("statut")) LIKE '%en cours%' THEN 'en cours'
+        WHEN LOWER(TRIM("statut")) LIKE '%fait%' THEN 'fait'
         ELSE 'non spécifié'
-    END AS libelle
-FROM staging.fournisseurs,
-     UNNEST(regexp_split_to_array(materiel, ', *')) AS materiel_value
-WHERE materiel IS NOT NULL;
--- A FAIRE nettoyage, éclatement et liaison fournisseur materiel
+     END ) AS signal_statut_nettoye
+FROM staging.signalements;
+-- Nettoyage signal_statut urgence 
+
+------------------------------------------TABLES NORMALES---------------------------------------
+
+INSERT INTO signalement (date_signalement)
+SELECT DISTINCT (
+    CASE
+        WHEN date LIKE '%.%.%'
+            THEN to_date (date, 'DD-MM-YYYY')
+        WHEN date LIKE '_____-__-__'
+        THEN to_date (date, 'DD-MM-YYYY')
+        ELSE NULL
+FROM staging.signalements;
+-- A FAIRE Nettoyage SIGNALEMENT
+
+INSERT INTO public.signalement (signale_par)
+SELECT DISTINCT (
+    CASE
+        WHEN urgence IS NULL OR TRIM("signale_par") = '' THEN 'inconnu'
+        WHEN LOWER(TRIM("signale_par")) LIKE '%habitant%' THEN 'habitant'
+        WHEN LOWER(TRIM("signale_par")) LIKE '%patrouille JM%' THEN 'patrouille'
+        WHEN LOWER(TRIM("signale_par")) LIKE '%email citoyen%' THEN 'email'
+        WHEN LOWER(TRIM("signale_par")) LIKE '%un passant%' THEN 'passant'
+        WHEN LOWER(TRIM("signale_par")) LIKE 'M. %' THEN TRIM(signale_par)
+        WHEN LOWER(TRIM("signale_par")) LIKE 'Mme %' THEN TRIM(signale_par)
+        WHEN LOWER(TRIM(public.unaccent("signale_par"))) LIKE '%concierge ecole%' THEN 'concierge'
+        ELSE 'non spécifié'
+     END ) AS signale_par_nettoye
+FROM staging.signalements;
+-- A FAIRE Nettoyage SIGNALEMENT
+
+INSERT INTO signalement (date_signalement)
+SELECT DISTINCT (
+    CASE
+        WHEN LOWER(TRIM("materiau")) LIKE '%bois%' THEN 'bois'
+        WHEN LOWER(TRIM(unaccent("materiau"))) LIKE '%metal%' THEN 'metal'
+        ELSE 'non spécifié'
+     END ) AS date_signalement_nettoye
+FROM staging.signalements;
+-- A FAIRE Nettoyage SIGNALEMENT
+
+INSERT INTO signalement (date_signalement)
+SELECT DISTINCT (
+    CASE
+        WHEN LOWER(TRIM("materiau")) LIKE '%bois%' THEN 'bois'
+        WHEN LOWER(TRIM(unaccent("materiau"))) LIKE '%metal%' THEN 'metal'
+        ELSE 'non spécifié'
+     END ) AS date_signalement_nettoye
+FROM staging.signalements;
+-- A FAIRE Nettoyage SIGNALEMENT
